2fefd81cc93509dfc019b85c324ee59657090fd3
Justin Obney <justinobney@gmail.com>
2018-04-24 15:29:45 -0500

    chore: refactor/implemebt table basics

--- src/App.js
+++ src/App.js
@@ -1,50 +1,301 @@
 import React, {Component} from 'react';
-import _ from 'lodash';
+import {find, indexOf} from 'lodash';
+import './App.css';
+
+// FAKE API STUFF
+
+function delayResponse(response, actionName) {
+  console.log('calling:', actionName);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(response);
+      console.log('completeing:', actionName, response);
+    }, 500);
+  });
+}
+
+let nextId = 9;
+const api = {
+  _data: [
+    {id: 1, size: 1, class: '150', schedule: '', laborFactor: 1.2},
+    {id: 2, size: 1, class: '300', schedule: '', laborFactor: 2.2},
+    {id: 3, size: 2, class: '150', schedule: '', laborFactor: 1.2},
+    {id: 4, size: 2, class: '300', schedule: '', laborFactor: 2.2},
+    {id: 5, size: 3, class: '150', schedule: '', laborFactor: 1.2},
+    {id: 6, size: 3, class: '300', schedule: '', laborFactor: 1.3},
+    {id: 7, size: 3, class: '450', schedule: '', laborFactor: 1.4},
+    {id: 8, size: 3, class: '450', schedule: '80', laborFactor: 1.4},
+  ],
+  get() {
+    return delayResponse(this._data, 'get');
+  },
+  add(record) {
+    const newRecord = {id: nextId++, ...record};
+    this._data = [...this._data, newRecord];
+    return delayResponse(newRecord, 'add');
+  },
+  update(record) {
+    this._data = this._data.map(item => {
+      if (item.id === record.id) {
+        return record;
+      }
+      return item;
+    });
+    return delayResponse(record, 'update');
+  },
+};
+
+class App extends Component {
+  render() {
+    return (
+      <LaborFactorProvider>
+        {({loading, data, ...rest}) =>
+          loading ? 'Loading...' : <LaborFactorTable data={data} {...rest} />
+        }
+      </LaborFactorProvider>
+    );
+  }
+}
+
+export class LaborFactorProvider extends Component {
+  state = {
+    loading: true,
+    data: [],
+  };
+
+  componentDidMount() {
+    this._fetchData();
+  }
+
+  _fetchData = () => {
+    this.setState({
+      loading: true,
+    });
+
+    api.get().then(data => {
+      this.setState({
+        loading: false,
+        data,
+      });
+    });
+  };
+
+  render() {
+    return this.props.children({...this.state, fetch: this._fetchData});
+  }
+}
+
+const getRecordKey = record =>
+  `${record.class}|${record.schedule}|${record.size}`;
 
 export function reducer(state, action = {}) {
   switch (action.type) {
-    case 'INCREMENT':
+    case 'EDIT_ROW':
       return {
         ...state,
-        count: state.count + (action.payload || 1),
+        editingRow: action.payload.row,
+        targetIndex: action.payload.targetIndex,
       };
-    case 'DECREMENT':
+    case 'ADD_CHANGED_RECORD':
       return {
         ...state,
-        count: state.count - 1,
+        mutations: {
+          ...state.mutations,
+          [getRecordKey(action.payload.record)]: action.payload.record,
+        },
+      };
+    case 'REMOVE_CHANGED_RECORD':
+      const mutations = {...state.mutations};
+      delete mutations[[getRecordKey(action.payload.record)]];
+      return {
+        ...state,
+        mutations,
       };
     default:
       return state;
   }
 }
 
-class App extends Component {
+export class LaborFactorTable extends Component {
   state = {
-    count: 1,
+    editingRow: null,
+    targetIndex: 0,
+    mutations: {},
+    sizes: {},
+    rows: {},
   };
+
+  static getDerivedStateFromProps(nextProps, state) {
+    const stateMap = nextProps.data.reduce(
+      (acc, item) => {
+        const key = `${item.class}|${item.schedule}`;
+
+        // add sizes to map
+        acc.sizes[item.size] = true;
+
+        // ensure class/schedule row exists
+        acc.rows[key] = acc.rows[key] || {
+          class: item.class,
+          schedule: item.schedule,
+        };
+
+        // add reference to class/schedule row mapping
+        // laborFactor record to size key
+        acc.rows[key][item.size] = item;
+
+        // return accumulator
+        return acc;
+      },
+      {sizes: {}, rows: {}}
+    );
+    return {
+      sizes: Object.keys(stateMap.sizes).map(x => parseInt(x, 10)),
+      rows: Object.keys(stateMap.rows).map(x => stateMap.rows[x]),
+    };
+  }
+
+  _dispatch = action => {
+    this.setState(state => {
+      console.groupCollapsed(`STATE CHANGING ::: ${action.type}`);
+      console.log('current state', state);
+      console.log('action', action);
+      const nextState = reducer(state, action);
+      console.log('next state', nextState);
+      console.groupEnd('STATE CHANGING');
+      return nextState;
+    });
+  };
+
+  _handleClick = (editing, row, targetIndex) => {
+    if (!editing) {
+      this._dispatch({
+        type: 'EDIT_ROW',
+        payload: {
+          row,
+          targetIndex,
+        },
+      });
+    }
+  };
+
+  _handleSaveChanges = () => {
+    const promises = Object.keys(this.state.mutations || {}).map(x => {
+      const mutation = this.state.mutations[x];
+      if (mutation.id) {
+        return api.update(mutation);
+      } else {
+        return api.add(mutation);
+      }
+    });
+
+    Promise.all(promises).then(() => {
+      this.props.fetch();
+    });
+  };
+
   render() {
     return (
-      <div>
-        <button
-          onClick={() => {
-            const action = {type: 'DECREMENT', payload: this.props.step || 1};
-            this.setState(state => reducer(state, action));
-          }}
-        >
-          {`+ ${this.props.step || 1}`}
-        </button>
-        <button
-          onClick={() => {
-            const action = {type: 'DECREMENT', payload: this.props.step || 1};
-            this.setState(state => reducer(state, action));
-          }}
-        >
-          {`- ${this.props.step || 1}`}
-        </button>
-        <h1>{this.state.count}</h1>
-      </div>
+      <React.Fragment>
+        <table>
+          <thead>
+            <tr>
+              <th>Class</th>
+              <th>Schedule</th>
+              {this.state.sizes.map(size => <th key={size}>{size}</th>)}
+            </tr>
+          </thead>
+          <tbody>{this.state.rows.map(this._renderRow)}</tbody>
+        </table>
+        <div>
+          Mutations:
+          <strong>
+            Adds ={' '}
+            {
+              Object.keys(this.state.mutations || {}).filter(
+                x => !this.state.mutations[x].id
+              ).length
+            }
+          </strong>
+          {` | `}
+          <strong>
+            Updates ={' '}
+            {
+              Object.keys(this.state.mutations || {}).filter(
+                x => !!this.state.mutations[x].id
+              ).length
+            }
+          </strong>
+          {!!Object.keys(this.state.mutations || {}).length && (
+            <button onClick={this._handleSaveChanges}>Save Changes</button>
+          )}
+        </div>
+      </React.Fragment>
     );
   }
+
+  _renderRow = row => {
+    const {editingRow, sizes} = this.state;
+    const editing = editingRow && find([row], editingRow);
+    const onClick = () => this._handleClick(editing, row, 0);
+    return (
+      <tr key={`${row.class}|${row.schedule}`}>
+        <td onClick={onClick}>{row.class}</td>
+        <td onClick={onClick}>{row.schedule}</td>
+        {sizes.map(size => this._renderCell({row, size, editing}))}
+      </tr>
+    );
+  };
+
+  _renderCell = ({row, size, editing}) => {
+    const record = row[size] || {
+      class: row.class,
+      schedule: row.schedule,
+      laborFactor: 0,
+      size,
+    };
+    const {sizes, targetIndex} = this.state;
+    const idx = indexOf(sizes, size);
+    const onClick = () => {
+      this._handleClick(editing, row, idx);
+    };
+    return (
+      <td key={`${row.class}|${row.schedule}|${size}`} onClick={onClick}>
+        {editing ? (
+          <input
+            onBlur={e => {
+              const laborFactor = parseFloat(e.target.value, 10);
+              if (laborFactor !== record.laborFactor) {
+                this._dispatch({
+                  type: 'ADD_CHANGED_RECORD',
+                  payload: {
+                    record: {
+                      ...record,
+                      laborFactor,
+                    },
+                  },
+                });
+              } else {
+                this._dispatch({
+                  type: 'REMOVE_CHANGED_RECORD',
+                  payload: {
+                    record: {
+                      ...record,
+                      laborFactor,
+                    },
+                  },
+                });
+              }
+            }}
+            name="laborFactor"
+            autoFocus={targetIndex === idx}
+            defaultValue={record.laborFactor}
+          />
+        ) : (
+          record.laborFactor
+        )}
+      </td>
+    );
+  };
 }
 
 export default App;
--- src/App.spec.js
+++ src/App.spec.js
@@ -1,49 +1,165 @@
 import React from 'react';
 import {create} from 'react-test-renderer';
-import App, {reducer} from './App';
+import {reducer, LaborFactorTable} from './App';
 
-it('should render component and its current state', () => {
-  const component = create(<App />);
-  const initialState = {count: 3};
-  component.root.instance.setState(initialState);
+it('component: LaborFactorTable => derive row and size data from props', () => {
+  const DATA = [
+    {id: 1, size: 1, class: '150', schedule: '1', laborFactor: 1.2},
+    {id: 2, size: 1, class: '300', schedule: '2', laborFactor: 2.2},
+    {id: 3, size: 2, class: '150', schedule: '', laborFactor: 1.2},
+  ];
+
+  expect(LaborFactorTable.getDerivedStateFromProps({data: DATA})).toEqual({
+    sizes: [1, 2],
+    rows: [
+      {
+        schedule: '1',
+        class: '150',
+        1: {id: 1, size: 1, class: '150', schedule: '1', laborFactor: 1.2},
+      },
+      {
+        schedule: '2',
+        class: '300',
+        1: {id: 2, size: 1, class: '300', schedule: '2', laborFactor: 2.2},
+      },
+      {
+        schedule: '',
+        class: '150',
+        2: {id: 3, size: 2, class: '150', schedule: '', laborFactor: 1.2},
+      },
+    ],
+  });
+});
+
+it('reducer: EDIT_ROW', () => {
+  // arrange
+  const state = {};
+  const action = {
+    type: 'EDIT_ROW',
+    payload: {
+      row: {},
+      targetIndex: 0,
+    },
+  };
+
+  // act
+  const nextState = reducer(state, action);
+
+  // assert
+  expect(nextState).toEqual({
+    editingRow: {},
+    targetIndex: 0,
+  });
+});
+
+it('reducer: ADD_CHANGED_RECORD', () => {
+  // arrange
+  const state = {mutations: {}};
+  const record = {class: '123', schedule: '123', size: 1};
+  const action = {
+    type: 'ADD_CHANGED_RECORD',
+    payload: {
+      record,
+    },
+  };
+
+  // act
+  const nextState = reducer(state, action);
+
+  // assert
+  expect(nextState).toEqual({
+    mutations: {
+      [`${record.class}|${record.schedule}|${record.size}`]: record,
+    },
+  });
+});
+
+it('reducer: REMOVE_CHANGED_RECORD', () => {
+  // arrange
+  const record = {class: '123', schedule: '123', size: 1};
+  const state = {
+    mutations: {
+      [`${record.class}|${record.schedule}|${record.size}`]: record,
+    },
+  };
+  const action = {
+    type: 'REMOVE_CHANGED_RECORD',
+    payload: {
+      record,
+    },
+  };
+
+  // act
+  const nextState = reducer(state, action);
+
+  // assert
+  expect(nextState).toEqual({
+    mutations: {},
+  });
+});
+
+it('render: default with data', () => {
+  const DATA = [
+    {id: 1, size: 1, class: '150', schedule: '80', laborFactor: 1.2},
+    {id: 2, size: 1, class: '300', schedule: '', laborFactor: 2.2},
+    {id: 3, size: 2, class: '150', schedule: '', laborFactor: 1.2},
+  ];
+
+  const component = create(<LaborFactorTable data={DATA} />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
-it('component should show increment size if provided', () => {
-  const component = create(<App step={5} />);
-  const initialState = {count: 3};
-  component.root.instance.setState(initialState);
+it('render: default with edited row', () => {
+  const DATA = [
+    {id: 1, size: 1, class: '150', schedule: '80', laborFactor: 1.2},
+    {id: 2, size: 1, class: '300', schedule: '', laborFactor: 2.2},
+    {id: 3, size: 2, class: '150', schedule: '', laborFactor: 1.2},
+  ];
+
+  const component = create(<LaborFactorTable data={DATA} />);
+  const editingRow = component.root.instance.state.rows[0];
+  component.root.instance.setState({editingRow});
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
-it('action: INCREMENT should increment state by 1', () => {
-  // arrange
-  const currentState = {count: 1};
-  const action = {type: 'INCREMENT'};
-  // act
-  const nextState = reducer(currentState, action);
-  // assert
-  expect(nextState).toEqual({count: 2});
+it('render: default with cell autofocus', () => {
+  const DATA = [
+    {id: 1, size: 1, class: '150', schedule: '80', laborFactor: 1.2},
+    {id: 2, size: 1, class: '300', schedule: '', laborFactor: 2.2},
+    {id: 3, size: 2, class: '150', schedule: '', laborFactor: 1.2},
+  ];
+
+  const component = create(<LaborFactorTable data={DATA} />);
+  const editingRow = component.root.instance.state.rows[0];
+  component.root.instance.setState({editingRow, targetIndex: 1});
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
 
-it('action: INCREMENT should increment state by payload', () => {
-  // arrange
-  const currentState = {count: 1};
-  const action = {type: 'INCREMENT', payload: 5};
-  // act
-  const nextState = reducer(currentState, action);
-  // assert
-  expect(nextState).toEqual({count: 6});
-});
+it('render: cell click triggers editing & autofocus', () => {
+  const DATA = [
+    {id: 1, size: 1, class: '150', schedule: '80', laborFactor: 1.2},
+    {id: 2, size: 1, class: '300', schedule: '', laborFactor: 2.2},
+    {id: 3, size: 2, class: '150', schedule: '', laborFactor: 1.2},
+  ];
 
-it('action: DECREMENT should increment state by -1', () => {
-  // arrange
-  const currentState = {count: 1};
-  const action = {type: 'DECREMENT'};
-  // act
-  const nextState = reducer(currentState, action);
-  // assert
-  expect(nextState).toEqual({count: 0});
+  const component = create(<LaborFactorTable data={DATA} />);
+  /**
+   * |===========================|
+   * | Class | Sched |  1  |  2  |
+   * |===========================|
+   * |  150  |  80   | 1.2 |  0  |
+   * |  300  |       | 2.2 |  0  |
+   * |  150  |       |  0  |  X  | <== 2.2
+   * |===========================|
+   */
+  component.root
+    .findByType('tbody')
+    .findAllByType('tr')[1]
+    .findAllByType('td')[3]
+    .props.onClick();
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
